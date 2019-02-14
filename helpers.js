f002876fe9f49380ec5ea2acaf7c4f38c2e1d1a1
Selwyn <talk@selwyn.cc>
2019-02-14 12:10:52 +0100

    Remove unused setImagePath

--- helpers.js
+++ helpers.js
@@ -12,7 +12,6 @@ module.exports = {
 function getAboutData(file) {
   return [file]
     .map(readMarkdown)
-    .map(setImagePath)
     .reduce(x => x[0]);
 }
 
@@ -20,23 +19,11 @@ function getCakesData(directory) {
   return fs
     .readdirSync(directory)
     .map(file => readMarkdown(path.join(directory, file)))
-    .map(setImagePath)
     .sort((a, b) => a.attributes.category.localeCompare(b.attributes.category));
+}
+
 }
 
 function readMarkdown(file) {
   return frontMatter(fs.readFileSync(file, 'utf8'));
-}
-
-function setImagePath(markdownData) {
-  return Object.assign({}, markdownData, {
-    attributes: Object.assign({}, markdownData.attributes, {
-      image: path.join(
-        '/',
-        'assets',
-        'media',
-        path.parse(markdownData.attributes.image).base
-      ),
-    }),
-  });
 }
